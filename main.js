83370861f7329aca81b7fcc8fddba7fb944c85e7
Yumpenik <45109195+Yumpenik@users.noreply.github.com>
2023-01-03 03:03:13 -0300

    Add files via upload
    update 1

--- main.js
+++ main.js
@@ -1,23 +1,51 @@
-$('#displayNone').click(function(e) {
+const botonPizza  = document.getElementById('botonPizza')
+const botonPapas  = document.getElementById('botonPapas')
+const botonTragos = document.getElementById('botonTragos')
+const botonBurger = document.getElementById('botonBurger')
+const botonMilas  = document.getElementById('botonMilas')
+
+const sectionPizza  = document.getElementById('pizza')
+const sectionPapas  = document.getElementById('papas')
+const sectionTragos = document.getElementById('tragos')
+const sectionBurger = document.getElementById('burger')
+const sectionMilas  = document.getElementById('milas')
+
+let condicionMostrar
+
+sectionPizza.style.display = 'none'
+sectionPapas.style.display = 'none'
+sectionTragos.style.display = 'none'
+sectionBurger.style.display = 'none'
+sectionMilas.style.display = 'none'
   
-    // Resetear, por si acaso has estado jugando con la otra propiedad
-    $('#hide-me').css('visibility', 'visible');
-    
-    if( $('#hide-me').is(":visible") ) {
-      $('#hide-me').css('display', 'none'); 
-    } else {
-      $('#hide-me').css('display', 'block');
-    }
-  });
+botonPizza.addEventListener('click', function(){Mostrar(1)})
+botonPapas.addEventListener('click', function(){Mostrar(2)})
+botonTragos.addEventListener('click', function(){Mostrar(3)})
+botonBurger.addEventListener('click', function(){Mostrar(4)})
+botonMilas.addEventListener('click', function(){Mostrar(5)})
+
+function Mostrar(a){
+
+condicionMostrar = a
+
+  sectionPizza.style.display = 'none'
+  sectionPapas.style.display = 'none'
+  sectionTragos.style.display = 'none'
+  sectionBurger.style.display = 'none'
+  sectionMilas.style.display = 'none'
   
-  $('#visibilityHidden').click(function(e) {
+  switch(a){
     
-    // Resetear, por si acaso has estado jugando con la otra propiedad
-    $('#hide-me').css('display', 'block');
+    case 1: {sectionPizza.style.display = 'flex'}
+    break
+    case 2: {sectionPapas.style.display = 'flex'}
+    break
+    case 3: {sectionTragos.style.display = 'flex'}
+    break
+    case 4: {sectionBurger.style.display = 'flex'}
+    break
+    case 5: {sectionMilas.style.display = 'flex'}
+    break
     
-    if( $('#hide-me').css('visibility') != 'hidden' ) {
-      $('#hide-me').css('visibility', 'hidden');
-    } else {
-      $('#hide-me').css('visibility', 'visible');
-    }
-  });
+  }
+}
